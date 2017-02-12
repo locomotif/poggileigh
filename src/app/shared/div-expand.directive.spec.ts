0b897ce4d187f6c3bead1704e1e076de9d8db83b
Bernardo Leigh <bpoggi@yahoo.com>
2017-02-12 12:18:23 -0800

    Add div expand directive. d3 animation of expanding and closing block nodes

---
+++ src/app/shared/div-expand.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DivExpandDirective } from './div-expand.directive';
+
+describe('DivExpandDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DivExpandDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/div-expand.directive.ts
@@ -0,0 +1,37 @@
+import { 
+    Directive,
+    ElementRef,
+    Input,
+    OnChanges,
+    Renderer,
+} from '@angular/core';
+import * as d3 from "d3/index";
+
+@Directive({
+    selector: '[appDivExpand]'
+})
+export class DivExpandDirective implements OnChanges {
+
+    private width: number = 320;
+    @Input("myDivExpandState") show: Boolean;
+
+    private d3div: any;
+
+    constructor(
+        private renderer: Renderer,
+        private el: ElementRef
+    ) { }
+
+    ngOnChanges(prop) {
+        if(!this.d3div)
+            this.d3div = d3.select(this.el.nativeElement);
+        this.animateDiv();
+    }
+    private animateDiv(): void {
+        if(this.show){
+            this.d3div.transition().duration(200).style("width", this.width + "px");
+        } else {
+            this.d3div.transition().duration(200).style("width", "0px");
+        }
+    }
+}
