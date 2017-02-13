578a90eced5908c8259c62b66c25adb7f1d36c82
Bernardo Leigh <bpoggi@yahoo.com>
2017-02-12 21:12:01 -0800

    Add npm script to execute a bash script that will execute ng build --prod, and move dist to service directory

---
+++ bin/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+##########################################################################
+# Execute ng build --prod
+# remove items related from build from the service dir
+# Copy contents from dist service directory
+#
+# author: Bernardo Leigh
+##########################################################################
+
+package_json_dir="`pwd`/"
+if ! [ -f "${package_json_dir}package.json" ]
+then
+    echo 'build.sh needs to run where package.json is located'
+    exit 1;
+fi
+
+ng build --prod
+set `ls dist`
+
+# remove old files
+for dist in $@
+do
+    postfix=${dist#*.*.}
+    prefix=${dist%%.*}
+
+    if [ -d "service/$dist" -o "$dist" = "index.html" ]
+    then
+        rm -rf service/$dist
+    else
+        ls service/ | while read f; do
+            post=${f#*.*.}
+            pre=${f%%.*}
+            if [ "$post$pre" = "$postfix$prefix" ]
+            then
+                rm -f "service/$f"
+            fi
+        done
+    fi
+done
+
+# copy new dist
+cp -r dist/ service/
