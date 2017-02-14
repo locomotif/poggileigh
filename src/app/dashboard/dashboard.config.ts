3827b552d84fb81f3c7fb871a675e378cf1d44b5
Bernardo Leigh <bpoggi@yahoo.com>
2017-02-14 11:30:50 -0800

    fix grammatical errors

--- src/app/dashboard/dashboard.config.ts
+++ src/app/dashboard/dashboard.config.ts
@@ -65,7 +65,7 @@ export const dashboardConfig = Object.freeze({
                 "min-height": "200px"
             },
             description: [
-                    "I transitioned to software engineering in 2009, after being layed off from a Fine Art Shipping company, during the mortgage crisis.  I took this opportuinity to develop a portion of me that I had abondoned when I decided to become a Fine Artist -- back in 1997.  I set a goal to immerse myself into this discipline for one year, before setting out to look for a position as a full stack LAMP engineer.  The journey began from the perspective of a Linux Administrator, and slowly bubbling up to client side technologies.  In December 2010, I applied to Parkside Lending LLC where I was employed for approximately 6 years.",
+                    "I transitioned to software engineering in 2009, after being laid off from a Fine Art Shipping company, during the mortgage crisis.  I took this opportunity to develop a portion of me that I had abandoned when I decided to become a Fine Artist.  I set a goal to immerse myself into this discipline for one year, before setting out to look for a position as a full stack LAMP engineer.  The journey began from the perspective of a Linux Administrator, and slowly bubbling up to client side technologies.  In December 2010, I applied to Parkside Lending LLC where I was employed for approximately 6 years.",
                     "Full Stack Engineering has evolved to a broad beast, which requires time to discover new technologies which fall into its spectrum.  Currently I been in the process of working with: AWS (CloudFormation), ansible, nginx, and angular2."
 
             ],
@@ -102,7 +102,7 @@ export const dashboardConfig = Object.freeze({
                 "min-height": "200px"
             },
             description: [
-                    "I find it difficult to balance life and work.  I tend to lean on the side of work, mostly because I enjoy the chanllanges that it presents me.  I tend to get lost in what I am working on and usualy set a goal to have it completed by a certain time.  This lends it self great to fine art and software development, because I tend to stay on the project until I meet the deadline.  This approach always unbalances any sense of work/life balance.  This is a work in progress!",
+                    "I find it difficult to balance life and work. I tend to lean on the side of work, mostly because I enjoy the challenges that it presents me. I tend to get lost in problem and usually set a goal to have it solved. This is great for fine art and software development during crunch time, but unfortunately is not sustainable to a balanced life and work.",
             ],
         },
 
@@ -135,7 +135,7 @@ export const dashboardConfig = Object.freeze({
                 "min-height": "200px"
             },
             description: [
-                    "I moved to SF in 1999 to study at the San Francisco Art Institue, after speding 4.5 years studing Industrial and Systems Engineering at the Unitveristy of Florida. It took a decade to realize that it was difficult for me to be a gallery artist.  I didn't have the business element required to work in a gallery setting.  I enjoy putting on a exhibtion of work, and was lucky to be able to show in some galleries.  My last show, was a group show in 2009, at Togonan Gallery.",
+                    "I moved to SF in 1999 to study at the San Francisco Art Institute, after spending 4.5 years studying Industrial and Systems Engineering at the University of Florida. It took a decade to realize that it was difficult for me to be a gallery artist.  I didn't have the business element required to work in a gallery setting.  I enjoy putting on art exhibitions, and was lucky to be able to show in some galleries.  My last show was a group show in 2009, at Togonan Gallery.",
             ],
         },
 
